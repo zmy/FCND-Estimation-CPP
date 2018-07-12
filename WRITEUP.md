570932a96a74f2c951b747041ab961164a242935
Mengyu Zhou <mezho@microsoft.com>
2018-07-12 20:57:55 +0800

    Step 2: Attitude Estimation

--- WRITEUP.md
+++ WRITEUP.md
@@ -1,6 +1,12 @@
 # Step 1: Sensor Noise
-By calculating standard deviations from the log files, we find that GPS X data follow `N(0, 0.7)` and accelerometer X data follow `N(0, 0.5)`.
-Thus we set `MeasuredStdDev_GPSPosXY = 0.7` and `MeasuredStdDev_AccelXY = 0.5`. Following is the simulation result:
+By calculating standard deviations from the log files, I find that GPS X data follow `N(0, 0.7)` and accelerometer X data follow `N(0, 0.5)`.
+Thus I set `MeasuredStdDev_GPSPosXY = 0.7` and `MeasuredStdDev_AccelXY = 0.5`. Following is the simulation result:
 <p align="center">
 <img src="images/s1.png" width="500"/>
+</p>
+
+# Step 2: Attitude Estimation
+By converting roll, pitch and yaw into quaternion and integrate body rate into the quaternion and then convert back, the nonlinear complementary filter is implemented as described in the document. Simulation result:
+<p align="center">
+<img src="images/s2.gif" width="500"/>
 </p>
--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -89,13 +89,13 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
   //       (Quaternion<float> also has a IntegrateBodyRate function, though this uses quaternions, not Euler angles)
 
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
-  // SMALL ANGLE GYRO INTEGRATION:
-  // (replace the code below)
-  // make sure you comment it out when you add your own code -- otherwise e.g. you might integrate yaw twice
 
-  float predictedPitch = pitchEst + dtIMU * gyro.y;
-  float predictedRoll = rollEst + dtIMU * gyro.x;
-  ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
+  Quaternion<float> qt = Quaternion<float>::FromEuler123_RPY(rollEst, pitchEst, ekfState(6));
+  qt.IntegrateBodyRate(gyro, dtIMU);
+
+  float predictedPitch = qt.Pitch();
+  float predictedRoll = qt.Roll();
+  ekfState(6) = qt.Yaw();	// yaw
 
   // normalize yaw to -pi .. pi
   if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
